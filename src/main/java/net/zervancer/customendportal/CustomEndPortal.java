a774e2d80e167a395dff4b81718ef75158affb75
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2025-03-04 11:20:53 +0100

    Init

---
+++ src/main/java/net/zervancer/customendportal/CustomEndPortal.java
@@ -0,0 +1,17 @@
+package net.zervancer.customendportal;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomEndPortal implements ModInitializer {
+	public static final String MOD_ID = "customendportal";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/zervancer/customendportal/CustomEndPortalDataGenerator.java
@@ -0,0 +1,11 @@
+package net.zervancer.customendportal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomEndPortalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
